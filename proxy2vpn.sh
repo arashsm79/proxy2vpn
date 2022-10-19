dce855b724f1bc09390f7091569ce01a11b74320
Arash Sal Moslehian <arashsm79@yahoo.com>
2022-10-19 22:19:56 +0330

    Remove all routes when stopped

--- proxy2vpn.sh
+++ proxy2vpn.sh
@@ -3,23 +3,26 @@
 print_usage() {
 	echo "
 Usage:
-    proxytovpn stop
+    proxytovpn stop ssh_addr
     proxytovpn start proxy_addr ssh_addr
 Example:
     proxytovpn start 127.0.0.1:8010 192.168.76.2
     "
 }
 
-current_default_route="$(route -n | grep -i 0.0.0.0 | head -n1 | awk '{print $2}')"
 
 case $1 in
 "stop")
-    if [ "$#" -ne 1 ]; then
+    if [ "$#" -ne 2 ]; then
         print_usage
     fi
 	killall badvpn-tun2socks
 	ifconfig tun0 down
-	route del default gw "$current_default_route" metric 6
+	route del default gw 10.0.0.2 metric 6
+    current_default_route="$(route -n | grep -i 0.0.0.0 | head -n1 | awk '{print $2}')"
+    route del "$2" gw "$current_default_route" metric 5
+    route del 8.8.8.8 gw "$current_default_route" metric 5
+	route del 1.1.1.1 gw "$current_default_route" metric 5
 	echo "VPN Disconnected"
 	exit
 ;;
@@ -39,6 +42,7 @@ case $1 in
 	#tun2socks without udpgw
     badvpn-tun2socks --tundev tun0 --loglevel error --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0  --socks-server-addr "$2" &
 
+    current_default_route="$(route -n | grep -i 0.0.0.0 | head -n1 | awk '{print $2}')"
 	#add route to the SSH
 	route add "$3" gw "$current_default_route" metric 5
 
